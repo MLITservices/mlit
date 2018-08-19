4d87e453d677a19ff54d4bbf358c212f034cd6a7
Mike Moye <mikemoye14@users.noreply.github.com>
2018-08-18 22:36:30 -0400

    Update server.js

--- server.js
+++ server.js
@@ -62,6 +62,7 @@ var initDb = function(callback) {
 */
 
 app.get('/', function (req, res) {
+    /*
   // try to initialize the db on every request if it's not already
   // initialized.
   if (!db) {
@@ -79,9 +80,12 @@ app.get('/', function (req, res) {
     });
   } else {
     res.render('index.html', { pageCountMessage : null});
-  }
+  }*/
+    
+    res.render('index.html');
 });
 
+    /*
 app.get('/pagecount', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
@@ -96,16 +100,17 @@ app.get('/pagecount', function (req, res) {
     res.send('{ pageCount: -1 }');
   }
 });
-
+*/
+    
 // error handling
 app.use(function(err, req, res, next){
   console.error(err.stack);
   res.status(500).send('Something bad happened!');
 });
 
-initDb(function(err){
-  console.log('Error connecting to Mongo. Message:\n'+err);
-});
+//initDb(function(err){
+  //console.log('Error connecting to Mongo. Message:\n'+err);
+//});
 
 app.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
